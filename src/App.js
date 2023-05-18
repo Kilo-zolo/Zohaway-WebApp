855aa6ed414d27e335593cd051b2f7983c16b3f4
Kilo-Zolo <krish5gupta25@gmail.com>
2023-05-18 19:11:42 +1000

    twind and bgimg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import bgImg from './assets/cybertree.png'
+
 
 function App() {
   return (
-    <div>
-
+    <div className='w-full h-full'>
+      <image src ={bgImg} />
     </div>
   );
 }
