69d9c73cd3100da0f2dd71c26f89ff188de4cde6
Kilo-zolo <Kilo-zolo@users.noreply.github.com>
2023-06-29 21:47:31 +1000

    deletes for GET

--- sql/GetDesserts.sql
+++
@@ -1,4 +0,0 @@
-SELECT Products.PROName, Products.IMG, Products.COST, Categories.CatName
-FROM Products
-INNER JOIN Categories ON Products.CatID=Categories.ID
-WHERE Categories.CatName = 'Desserts'
--- sql/GetProductbyCategory.sql
+++ sql/GetProductbyCategory.sql
@@ -1,4 +1,6 @@
+DECLARE @cn NVARCHAR(1000);
+set @cn = 'Desserts'
 SELECT Products.PROName, Products.IMG, Products.COST, Categories.CatName
 FROM Products
 INNER JOIN Categories ON Products.CatID=Categories.ID
-WHERE Categories.CatName = 'Beverages'
+WHERE Categories.CatName = @cn
